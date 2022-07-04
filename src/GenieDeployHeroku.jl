b4f0f6ab169ce68e3e223fed62a58c7eef4a8fe0
Adrian Salceanu <adrian.salceanu@gmail.com>
2022-07-04 12:56:28 +0300

    API for managing the stack + Nothing return types

--- src/GenieDeployHeroku.jl
+++ src/GenieDeployHeroku.jl
@@ -10,8 +10,10 @@ const HEROKU = @static Sys.iswindows() ? `heroku.cmd` : `heroku`
 
 Returns list of apps available on Heroku account.
 """
-function apps()
+function apps() :: Nothing
   `$HEROKU apps` |> GenieDeploy.run
+
+  nothing
 end
 
 """
@@ -20,8 +22,36 @@ end
 Runs the `heroku create` command to create a new app in the indicated region.
 See https://devcenter.heroku.com/articles/heroku-cli-commands#heroku-apps-create-app
 """
-function createapp(appname::String; region::String = "us")
+function createapp(appname::String; region::String = "us", stack::Union{String,Symbol,Nothing} = :container) :: Nothing
   `$HEROKU create $(appname) --region $region` |> GenieDeploy.run
+  stack !== nothing && setstack(appname, stack)
+
+  nothing
+end
+
+
+"""
+    stack(appname::String)
+
+Determine which stack your app is using and list available stacks.
+"""
+function stack(appname::String) :: Nothing
+  `$HEROKU stack -a $(appname)` |> GenieDeploy.run
+
+  nothing
+end
+
+
+"""
+    setstack(appname::String, stack::Union{String,Symbol})
+
+Change the stack your app will use for the next deploy. Default stack is `container`.
+See `stack(appname)` for available stacks.
+"""
+function setstack(appname::String, stack::Union{String,Symbol} = :container) :: Nothing
+  `$HEROKU stack:set $(stack) -a $(appname)` |> GenieDeploy.run
+
+  nothing
 end
 
 
@@ -31,8 +61,10 @@ end
 Invokes the `heroku container:push` which builds, then pushes Docker images to deploy your Heroku app.
 See https://devcenter.heroku.com/articles/heroku-cli-commands#heroku-container-push
 """
-function push(appname::String; apptype::String = "web")
+function push(appname::String; apptype::String = "web") :: Nothing
   `$HEROKU container:push $apptype -a $appname` |> GenieDeploy.run
+
+  nothing
 end
 
 
@@ -42,8 +74,10 @@ end
 Invokes the `keroku container:release` which releases previously pushed Docker images to your Heroku app.
 See https://devcenter.heroku.com/articles/heroku-cli-commands#heroku-container-push
 """
-function release(appname::String; apptype::String = "web")
+function release(appname::String; apptype::String = "web") :: Nothing
   `$HEROKU container:release $apptype -a $appname` |> GenieDeploy.run
+
+  nothing
 end
 
 
@@ -53,8 +87,10 @@ end
 Invokes the `heroku open` command which open the app in a web browser.
 See https://devcenter.heroku.com/articles/heroku-cli-commands#heroku-apps-open-path
 """
-function open(appname::String)
+function open(appname::String) :: Nothing
   `$HEROKU open -a $appname` |> GenieDeploy.run
+
+  nothing
 end
 
 
@@ -64,8 +100,10 @@ end
 Invokes the `heroku container:login` to log in to Heroku Container Registry,
 See https://devcenter.heroku.com/articles/heroku-cli-commands#heroku-container-login
 """
-function login()
+function login() :: Nothing
   `$HEROKU container:login` |> GenieDeploy.run
+
+  nothing
 end
 
 
@@ -75,8 +113,10 @@ end
 Display recent heroku log output.
 https://devcenter.heroku.com/articles/heroku-cli-commands#heroku-logs
 """
-function logs(appname::String; lines::Int = 1_000)
+function logs(appname::String; lines::Int = 1_000) :: Nothing
   `$HEROKU logs --tail -a $appname -n $lines` |> GenieDeploy.run
+
+  nothing
 end
 
 end
